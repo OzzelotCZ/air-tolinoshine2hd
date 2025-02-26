7fef824e99ed59defa07c0fcfa3d1e1a171ac981
Ozzelot <vladacvach@seznam.cz>
2025-02-26 19:39:26 +0100

    Update backlight in suspend script

--- suspend.sh
+++ suspend.sh
@@ -2,8 +2,7 @@
 
 FILE=/tmp/sleeping
 if [ -f "$FILE" ]; then
-	brightnessctl -d "backlight_warm" s $(sed -n 0p $FILE)
-	brightnessctl -d "backlight_cold" s $(sed -n 1p $FILE)
+	brightnessctl -d "backlight_warm"  $(sed -n 0p $FILE)
 	rm $FILE
         notify-send "Welcome back" "Waking up..." -t 1000
         echo 1-0024 | sudo tee /sys/bus/i2c/drivers/cyttsp5/unbind
@@ -11,12 +10,10 @@ if [ -f "$FILE" ]; then
         echo 1-0024 | sudo tee /sys/bus/i2c/drivers/cyttsp5/bind
 else
 	touch $FILE
-	brightnessctl -d "backlight_warm" g > $FILE
-	brightnessctl -d "backlight_cold" g > $FILE
-	brightnessctl -d "backlight_warm" s 0
-	brightnessctl -d "backlight_cold" s 0
+	brightnessctl -d "backlight" g > $FILE
+	brightnessctl -d "backlight" s 0
 	notify-send "Sleeping" "zzz" -t 2000
-	sleep 1
+	sleep 
 	sudo pm-suspend
 fi
 
